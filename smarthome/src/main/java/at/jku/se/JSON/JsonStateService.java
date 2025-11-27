bafab1297f70ae1aac82eb9e7752ed443ddfaf93
paultraxler <paul.traxler02@gmail.com>
2025-11-27 10:25:35 +0100

    Implement Json Load/Save #35  | time: 270m

---
+++ smarthome/src/main/java/at/jku/se/JSON/JsonStateService.java
@@ -0,0 +1,62 @@
+package JSON;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class JsonStateService {
+    private static JsonStateService instance;
+    private final Path path = Paths.get("database.json");
+    private final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+    private JsonStateService() {}
+
+    public static JsonStateService getInstance() {
+        if (instance == null) {
+            instance = new JsonStateService();
+        }
+        return instance;
+    }
+
+    private void createIfNotExist(){
+        try {
+            Files.readString(path);
+        }catch (IOException e) {
+            try {
+                Files.writeString(path, "{}");
+                System.out.println("Datenbank-Datei wurde neu erstellt.");
+            } catch (IOException ex) {
+                throw new RuntimeException("Konnte die Datenbank-Datei nicht erstellen!", ex);
+            }
+        }
+    }
+    public AppState load() {
+        try{
+            createIfNotExist();
+
+            String json = Files.readString(path);
+            AppState state = gson.fromJson(json, AppState.class);
+
+            if(state == null){
+                state = new AppState();
+            }
+            return state;
+        }catch (Exception e) {
+            throw new RuntimeException("Fehler beim Laden der Datenbank!", e) ;
+        }
+    }
+    public void save(AppState state) {
+        try{
+            String json = gson.toJson(state);
+            Files.writeString(path, json);
+        }catch (Exception e) {
+            throw new RuntimeException("Fehler beim Speichern der Datenbank!", e) ;
+        }
+    }
+
+
+}
